8720ca07aad0f299675d23e46bf977bb3a5db48b
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-02-29 16:10:32 -0500

    Add route

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -8,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class InicioComponent implements OnInit {
   peso = 60;
   edad = 25;
+  altura = 170;
+  sexo = 'Masculino';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  calcularBMI() {
+    const bmi = this.peso / Math.pow(this.altura / 100, 2);
+    console.log(bmi);
+
+    this.router.navigate(['/resultado', bmi.toFixed(1)]);
+
+  }
+
+  cambiarAltura(event: any) {
+    this.altura = event.target.value;
+  }
+
+  masculino() {
+    this.sexo = 'Masculino';
+  }
+  femenino() {
+    this.sexo = 'Femenino';
   }
 
 }
--- src/app/components/resultado/resultado.component.ts
+++ src/app/components/resultado/resultado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-resultado',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultadoComponent implements OnInit {
 
-  constructor() { }
+  bmi: number = 0;
+  resultado = '';
+  interpretacion = '';
+
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.bmi = +this.route.snapshot.paramMap.get('valor')!;
+
+    this.getResultado();
+
+  }
+
+  getResultado() {
+
+    if (this.bmi >= 25) {
+      this.resultado = 'Exceso de peso';
+      this.interpretacion = 'Tienes un peso corporal superior al normal. Intente hacer más ejercicio'
+    } else if (this.bmi >= 18.5) {
+      this.resultado = 'Normal';
+      this.interpretacion = 'Tienes un peso corporal normal. ¡Buen trabajo!'
+    } else {
+      this.resultado = 'Bajo peso';
+      this.interpretacion = 'Tienes un peso corporal más bajo de lo normal. Puedes comer un poco más'
+    }
   }
 
 }
